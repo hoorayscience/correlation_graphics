cc9f3ba0c8ffcd95b94308c3e1c12767df6c763f
unknown <you@example.com>
2020-11-05 13:15:16 -0800

    Start of script

---
+++ correlation_analysis.R
@@ -0,0 +1,14 @@
+##Code to assumption check and analyze regression##
+##Ellie Wenger##
+##11/5/2020##
+
+##Load Packages##
+library(dplyr)
+library(ggpubr)
+library(readr)
+
+##Load tidy data###
+PAH_Data <- read_csv("PAH_Data.csv")
+head(PAH_Data)
+
+##
