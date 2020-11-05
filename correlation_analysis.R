421829888abd45239baa29b3ffdffc1a55aa482b
unknown <you@example.com>
2020-11-05 15:54:04 -0800

    started plot code, use code at home to figure out

--- correlation_analysis.R
+++ correlation_analysis.R
@@ -9,6 +9,11 @@ library(readr)
 library(rstatix)
 library(tidyverse)
 library(PerformanceAnalytics)
+library(stringr)
+library(reshape2)
+library(ggplot2)
+library(ggthemes)
+library(pander)
 
 ##Load tidy data###
 PAH_Data <- read_csv("PAH_Data.csv")
@@ -41,9 +46,22 @@ log.normality
 trans.numerical.vars$`Benzo(k)fluoranthene` <- numerical.vars$`Benzo(k)fluoranthene`^(1/4)
 trans.numerical.vars$`Benz(a)anthracene` <- numerical.vars$`Benz(a)anthracene`^(1/4)
 
+##use correlation chart to check out histograms and pearson correlations
 correlation <- trans.numerical.vars[, c(1:16)]
-chart.Correlation(correlation, histogram=TRUE, pch=19)
+chart.Correlation(correlation, histogram=TRUE, method=c("pearson"), pch=19)
 
+##for loop for correlation tests. does not work yet
 
+for (i in 1:length(trans.numerical.vars)) {
+  a <- cor.test(trans.numerical.vars$`AhR BEQ (ng TCDD/g)`, trans.numerical.vars[,i])
+  print(paste(colnames(trans.numerical.vars)[i], " est:", a$estimate, " p=value:", a$p.value))
+}
 
+##for loop to plot all congeners
+
+for(i in 2:ncol(data)) {                              # ggplot within for-loop
+  ggplot(data, aes(x = x, y = data[ , i])) +
+    geom_point()
+  Sys.sleep(2)
+}
 
